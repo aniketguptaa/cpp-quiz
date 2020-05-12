bf05f82255211ad73ec251bd6ee61549ee6564f3
Aniket Gupta <63919139+aniketguptaa@users.noreply.github.com>
2020-05-12 21:29:31 +0530

    Add files via upload

---
+++ starpattern1.cpp
@@ -0,0 +1,95 @@
+// *
+// **
+// ***
+// ****
+// *****
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+// int num = 5;
+// int i = 1, j = 1;
+// for ( i = 1; i <= num; ++i)
+// {
+//     for ( j = 1; j <= i; ++j)
+//     {
+//         cout<<"*";
+//     }
+//     cout<<"\n";
+// }
+// return 0;
+// }
+// ----------------------------------------------
+
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+// int num = 5;
+// int i = 1, j = 1;
+// for ( i = 1; i <= num; ++i)
+// {
+//     for ( j = 1; j <= i; ++j)
+//     {
+//         cout<<"*";
+//     }
+//     cout<<"\n";
+// }
+// return 0;
+// }
+
+// ---------------------------------
+// *****
+// ****
+// ***
+// **
+// *
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+// int num = 5;
+// int i = 1, j = 1;
+// for ( i = num; i >= 1; --i)
+// {
+//     for ( j = 1; j <= i; ++j)
+//     {
+//         cout<<"*";
+//     }
+//     cout<<"\n";
+// }
+// return 0;
+// }
+// // ----------------------------------
+
+// 12345
+// 1234
+// 123
+// 12
+// 1
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+// int num = 5;
+// int i = 1, j = 1;
+// for ( i = num; i >= 1; --i)
+// {
+//     for ( j = 1; j <= i; ++j)
+//     {
+//         cout<<j;
+//     }
+//     cout<<"\n";
+// }
+// return 0;
+// }
+
+
+
